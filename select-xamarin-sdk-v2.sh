e61807eb24bea7cdfdd5b503a5bbaee425a1d948
mikhailshaganov <81769678+mikhailshaganov@users.noreply.github.com>
2021-04-29 11:31:17 +0300

    Update select-xamarin-sdk-v2.sh

--- select-xamarin-sdk-v2.sh
+++ select-xamarin-sdk-v2.sh
@@ -2,53 +2,52 @@
 
 echo "Set SDK"
 FOLDERS_LIST=(
-        '/Library/Frameworks/Mono.framework/Versions'
-        '/Library/Frameworks/Xamarin.iOS.framework/Versions'
-        '/Library/Frameworks/Xamarin.Android.framework/Versions'
-        '/Library/Frameworks/Xamarin.Mac.framework/Versions'
-    )
+  '/Library/Frameworks/Mono.framework/Versions'
+  '/Library/Frameworks/Xamarin.iOS.framework/Versions'
+  '/Library/Frameworks/Xamarin.Android.framework/Versions'
+  '/Library/Frameworks/Xamarin.Mac.framework/Versions'
+)
 
-set_current_folder ()
-{
-    local framework=$1 
-    local version=$2
-    local folderListPosition=$3
+get_framework_path() {
+  framework=$1
+  case $key in
+  mono) return '/Library/Frameworks/Mono.framework/Versions' ;;
+  ios) return '/Library/Frameworks/Xamarin.iOS.framework/Versions' ;;
+  android) return '/Library/Frameworks/Xamarin.Android.framework/Versions' ;;
+  mac) return '/Library/Frameworks/Xamarin.Mac.framework/Versions' ;;
+  *) ;;
+  esac
+}
 
-    echo "framework = $framework, version = $version, folderListPosition = $folderListPosition"
+set_current_folder() {
+  local framework=$1
+  local version=$2
+  local folderListPosition=$3
 
-    if [ ! -z ${framework} ]; 
-    then 
-      IFS='.'
-      read -a arr <<< "$version"
-      echo "split arr: ${#arr[@]}"
-      if [[ ! ${#arr[@]} -eq 2 ]]; 
-      then 
-        echo "Wrong framework's versions."
-        return
-      fi
+  echo "framework = $framework, version = $version"
+
+  if [ ! -z ${framework} ]; then
+    local countDigit=$(echo "${version}" | grep -o "\." | grep -c "\.")
+    if [[ ! countDigit -eq 1 ]]; then
+      echo "Wrong framework's versions."
+      return
     fi
+  fi
 
-    FOLDER=${FOLDERS_LIST[folderListPosition]}
-    echo "Set Current folder for ${FOLDER}"
-    echo "Current new folder ${FOLDER}/${version} ${FOLDER}/Current"
-    sudo rm -f ${FOLDER}/Current
-    sudo ln -s ${FOLDER}/${version} ${FOLDER}/Current
-}    
+  FOLDER=get_framework_path $framework
+  sudo rm -f ${FOLDER}/Current
+  sudo ln -s "${FOLDER}/${version}" "${FOLDER}/Current"
+}
 
+for arg in "$@"; do
+  key=$(echo $arg | cut -f1 -d=)
+  value=$(echo $arg | cut -f2 -d=)
 
-for arg in "$@"
-do
-
-#Separate argument name and value
-key=$(echo $arg | cut -f1 -d=)
-value=$(echo $arg | cut -f2 -d=)
-
-#Print message based on argument’s name
-case $key in
-mono) set_current_folder $key $value 1;;
-ios) set_current_folder $key $value 2;;
-android) set_current_folder $key $value 3;;
-mac) set_current_folder $key $value 4;;
-*)
-esac
+  case $key in
+  mono) set_current_folder $key $value 1 ;;
+  ios) set_current_folder $key $value 2 ;;
+  android) set_current_folder $key $value 3 ;;
+  mac) set_current_folder $key $value 4 ;;
+  *) ;;
+  esac
 done
